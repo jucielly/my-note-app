c78f5b0b4575f9cafce7a6bc8b38860f10503e89
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-04-30 14:38:19 -0300

    notes container

--- src/components/organisms/NoteList/index.tsx
+++ src/components/organisms/NoteList/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../store/reducers'
 import Note from '../../molecules/Note'
@@ -8,11 +9,20 @@ import Note from '../../molecules/Note'
 const NoteList: React.FC = () => {
     const notes = useSelector((state: RootState) => state.notes)
     return (
-        <div>
-            {notes.map((note,key) => <Note noteValue={note.note} key={key} /> )}
-        </div>
+        <Container>
+            {notes.map((note, key) => <Note noteValue={note.note} key={key} />)}
+        </Container>
+
+
     )
 }
 
 
 export default NoteList
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: stretch;
+`
